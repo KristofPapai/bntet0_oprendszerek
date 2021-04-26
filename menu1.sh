bbd1f96c3cbf820dd1fbcd795ae475f3981a51f8
Ppai Kristof <kristofpapai@gmail.com>
2021-04-26 17:25:00 +0200

    masodik push?

--- menu1.sh
+++ menu1.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].type  > type.txt
 curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].serial  > capsules.txt
+curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].reuse_count  > reuse.txt
+curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].last_update  > update.txt
+curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].status  > status.txt
+
+
 
 echo "Menü:"
 echo "-----------------------------------"
