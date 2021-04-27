010eeb697b9ec97986f94dc29e6fe36f56009153
Ppai Kristof <kristofpapai@gmail.com>
2021-04-27 19:07:36 +0200

    menu1.sh 1.0

--- menu1.sh
+++ menu1.sh
@@ -5,6 +5,11 @@ curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].reuse_count  > reuse.tx
 curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].last_update  > update.txt
 curl "https://api.spacexdata.com/v4/capsules" |jq -r .[].status  > status.txt
 
+curl "https://api.spacexdata.com/v4/cores" |jq -r .[].serial  > serialB.txt
+curl "https://api.spacexdata.com/v4/cores" |jq -r .[].block  > block.txt
+curl "https://api.spacexdata.com/v4/cores" |jq -r .[].reuse_count  > reuseB.txt
+curl "https://api.spacexdata.com/v4/cores" |jq -r .[].status  > statusB.txt
+curl "https://api.spacexdata.com/v4/cores" |jq -r .[].last_update  > updateB.txt
 
 
 echo "Menü:"
@@ -15,10 +20,14 @@ read input2
 
 
 if [[ "$input2" == 1 ]]; then
-paste type.txt serial.txt reuse.txt status.txt update.txt >> test.txt
+paste type.txt serial.txt reuse.txt status.txt update.txt >> kapsz.txt
 echo "Típus	széria szám	használatok száma	státusz		utolsó fejlemény"
-cat test.txt
+cat kapsz.txt
 
+elif [[ "input2" == 2 ]]; then
+paste serialB.txt block.txt reuseB.txt statusB.txt updateB.txt >> falc.txt
+echo "Falcon sorozatszám     Block     használatok száma       státusz         utolsó fejlemény"
+cat falc.txt
 
 else
 echo "valami más"
