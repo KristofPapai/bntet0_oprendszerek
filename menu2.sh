4e34b7384cfdb54da9e36e99c2371a0609b71798
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 13:34:28 +0200

    Fin

--- menu2.sh
+++ menu2.sh
@@ -6,9 +6,6 @@ curl "https://api.spacexdata.com/v4/history" |jq -r .[].details > details.txt
 paste title.txt date.txt > history.txt
 
 clear
-#echo "---------------------------------------------"
-#echo "Esemény///Esemény időpontja"
-#echo "---------------------------------------------"
 sed -i '1i\----------		----------' history.txt
 
 sed -i '1i\Esemény	Esemény időpontja' history.txt
