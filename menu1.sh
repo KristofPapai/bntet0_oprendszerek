6968fc36168cc75a253cd50c55f4a42feb70de42
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 11:42:35 +0200

    no more txts

--- menu1.sh
+++ menu1.sh
@@ -21,14 +21,31 @@ read input2
 
 if [[ "$input2" == 1 ]]; then
 paste type.txt serial.txt reuse.txt status.txt update.txt > kapsz.txt
-echo "Típus	széria szám	használatok száma	státusz		utolsó fejlemény"
+echo "--------------------------------------------------------------------"
+echo "Típus///széria szám///használatok száma///státusz///utolsó fejlemény"
+echo "--------------------------------------------------------------------"
 cat kapsz.txt
 
 elif [[ "$input2" == 2 ]]; then
 paste serialB.txt block.txt reuseB.txt statusB.txt updateB.txt > falc.txt
-echo "Falcon sorozatszám     Block     használatok száma       státusz         utolsó fejlemény"
+echo "---------------------------------------------------------------------------"
+echo "Falcon sorozatszám///Block///használatok száma///státusz///utolsó fejlemény"
+echo "---------------------------------------------------------------------------"
 cat falc.txt
 
 else
 echo "valami más"
 fi
+
+rm kapsz.txt
+rm type.txt
+rm serial.txt
+rm reuse.txt
+rm status.txt
+rm update.txt
+rm falc.txt
+rm serialB.txt
+rm block.txt
+rm reuseB.txt
+rm statusB.txt
+rm updateB.txt
