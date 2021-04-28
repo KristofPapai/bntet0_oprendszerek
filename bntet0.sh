b2723f2959c906dab54389b10a7a74fde4d55f36
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 13:35:12 +0200

    fin

--- bntet0.sh
+++ bntet0.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+clear
 echo "A SpaceX API program"
 echo "Készitette: Pápai Kristóf Levente - BNTET0"
 # curl "https://api.spacexdata.com/v4/capsules"
 echo "------------------------------------------"
 echo "Menü:"
+echo "------------------------------"
 echo "1. SpaceX űrhajók"
 echo "2. SpaceX történelmi események"
 echo "3. SpaceX legényég"
+echo "4. SpaceX Rakéták"
+echo "------------------------------------------"
 echo "Kérem írja be a menüpont számát amelyiket ki szeretné listázni."
 read input1
 
@@ -14,10 +18,12 @@ if [[ "$input1" == 1 ]]; then
 echo "1.es menü lett kiválasztva"
 bash menu1.sh
 elif [[ "$input1" == 2 ]]; then
-echo "2.es menü lett kiválasztva"
+echo "2-es menü lett kiválasztva"
 bash menu2.sh
 elif [[ "$input1" == 3 ]]; then
-echo "3.as menü lett kiválasztva"
+echo "3-as menü lett kiválasztva"
+elif [[ "$input1" == 4 ]]; then
+echo "4-es menü lett kiválasztva"
 else
 echo "helytelen bevitel"
 fi
