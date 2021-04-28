3b4ec172d7b6ee439734d1a02aff669a42f2b321
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 14:23:37 +0200

    fin menu3

--- bntet0.sh
+++ bntet0.sh
@@ -10,6 +10,7 @@ echo "1. SpaceX űrhajók"
 echo "2. SpaceX történelmi események"
 echo "3. SpaceX legényég"
 echo "4. SpaceX Rakéták"
+echo "0. kilépés"
 echo "------------------------------------------"
 echo "Kérem írja be a menüpont számát amelyiket ki szeretné listázni."
 read input1
@@ -22,8 +23,11 @@ echo "2-es menü lett kiválasztva"
 bash menu2.sh
 elif [[ "$input1" == 3 ]]; then
 echo "3-as menü lett kiválasztva"
+bash menu3.sh
 elif [[ "$input1" == 4 ]]; then
 echo "4-es menü lett kiválasztva"
+elif [[ "$input1" == 0 ]]; then
+clear
 else
 echo "helytelen bevitel"
 fi
