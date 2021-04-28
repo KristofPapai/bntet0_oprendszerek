0bdffa78070c6d08cba3db87255cfde25d67a1f2
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 13:12:43 +0200

    spacing not good yet

--- menu2.sh
+++ menu2.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-curl "https://api.spacexdata.com/v4/launches/past" |jq -r .[].date_utc  > date.txt
+curl "https://api.spacexdata.com/v4/history" |jq -r .[].title > title.txt
+curl "https://api.spacexdata.com/v4/history" |jq -r .[].event_date_utc > date.txt
+curl "https://api.spacexdata.com/v4/history" |jq -r .[].details > details.txt
 
+paste title.txt date.txt > history.txt
+
+echo "---------------------------------------------"
+echo "Esemény///Esemény időpontja"
+echo "---------------------------------------------"
+cat history.txt
+
+rm title.txt
+rm date.txt
+rm details.txt
+rm history.txt
