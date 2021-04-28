e601e191a457f58e6dab4cf03ca235d51f783d23
Ppai Kristof <kristofpapai@gmail.com>
2021-04-28 14:24:12 +0200

    fin menu2

--- menu2.sh
+++ menu2.sh
@@ -11,6 +11,8 @@ sed -i '1i\----------		----------' history.txt
 sed -i '1i\Esemény	Esemény időpontja' history.txt
 
 cat history.txt | column -t -s$'\t'
+read input2
+bash bntet0.sh
 
 rm title.txt
 rm date.txt
